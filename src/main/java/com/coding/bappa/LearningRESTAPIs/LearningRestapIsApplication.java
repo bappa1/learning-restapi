90060c45c347601d4474d7e967c6176433129959
Bappa Pal <bappapal121@gmail.com>
2025-10-07 22:26:42 +0530

    Store this code in Git

---
+++ src/main/java/com/coding/bappa/LearningRESTAPIs/LearningRestapIsApplication.java
@@ -0,0 +1,14 @@
+package com.coding.bappa.LearningRESTAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningRestapIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningRestapIsApplication.class, args);
+		System.out.println("This is The main method::::");
+	}
+
+}
