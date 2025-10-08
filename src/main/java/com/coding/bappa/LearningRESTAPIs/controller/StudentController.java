a73b55f6e7d5b4e4de51793064cb4c9caa41ae4b
Bappa Pal <bappapal121@gmail.com>
2025-10-08 06:28:53 +0530

    Adding Controller and DTO files

---
+++ src/main/java/com/coding/bappa/LearningRESTAPIs/controller/StudentController.java
@@ -0,0 +1,13 @@
+package com.coding.bappa.LearningRESTAPIs.controller;
+
+import com.coding.bappa.LearningRESTAPIs.dto.StudentDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @GetMapping("/student")
+    public StudentDto getStudent(){
+        return new StudentDto(1L, "Bappa Pal", "bappa.pal@g,ail.com");
+    }
+}
