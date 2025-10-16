e1a0246d1a5cbc0a38c8c9f3ffae63c65fc550b9
Bappa Pal <bappapal121@gmail.com>
2025-10-16 21:27:50 +0530

    Writing JPQL code

--- src/test/java/com/coding/bappa/LearningRESTAPIs/LearningRestapIsApplicationTests.java
+++ src/test/java/com/coding/bappa/LearningRESTAPIs/LearningRestapIsApplicationTests.java
@@ -1,13 +1,26 @@
 package com.coding.bappa.LearningRESTAPIs;
 
+import com.coding.bappa.LearningRESTAPIs.entity.Patient;
+import com.coding.bappa.LearningRESTAPIs.repository.PatientRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class LearningRestapIsApplicationTests {
-
+	@Autowired
+	private PatientRepository patientRepository;
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	public void patientByCity(){
+		List<Patient> patients = patientRepository.findByCity("Kochi");
+		for(Patient patient:patients){
+			System.out.println("Patient is:" + patient);
+		}
 	}
 
 }
